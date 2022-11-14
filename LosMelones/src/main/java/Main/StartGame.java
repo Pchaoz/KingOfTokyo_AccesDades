c23a22e4003a78b50ff0794dba9a5cd16eb4a82d
raulmartincaro <raul.martincaro@gmail.com>
2022-11-14 19:38:32 +0100

    unas pocas cosas
    unas poquillas cosas

---
+++ LosMelones/src/main/java/Main/StartGame.java
@@ -0,0 +1,29 @@
+package Main;
+
+public class StartGame {
+	
+	private int nJugadors;
+	
+	
+
+	public StartGame(int nJugadors) {
+		super();
+		this.nJugadors = nJugadors;
+		GenerarPartida();
+	}
+
+	public int getnJugadors() {
+		return nJugadors;
+	}
+
+	public void setnJugadors(int nJugadors) {
+		this.nJugadors = nJugadors;
+	}
+	
+
+	private void GenerarPartida() {
+		
+		
+	}
+
+}

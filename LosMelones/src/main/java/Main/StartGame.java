c42e0bf19e9212ce9e1889567ea0aa5ad1e48d0c
raulmartincaro <raul.martincaro@gmail.com>
2022-11-15 19:26:02 +0100

    ya se selecciona el jugador inicial
    jugador incial y tiradas funcionales
    falta analizar tiradas.

--- LosMelones/src/main/java/Main/StartGame.java
+++ LosMelones/src/main/java/Main/StartGame.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -8,10 +9,8 @@ import DAO.JugadorDAO;
 import Model.Jugador;
 
 public class StartGame {
-	
+
 	private int nJugadors;
-	
-	
 
 	public StartGame(int nJugadors) {
 		super();
@@ -26,38 +25,36 @@ public class StartGame {
 	public void setnJugadors(int nJugadors) {
 		this.nJugadors = nJugadors;
 	}
-	
-	
+
 	private void Jugar() {
-		boolean FiPartida=false;
-		while(!FiPartida) {
-			JugadorDAO jugadorDao = new JugadorDAO();
-			List<Jugador> jugadors = jugadorDao.listar();
-			SeleccionarTorn();
+		boolean FiPartida = false;
+
+		JugadorDAO jugadorDao = new JugadorDAO();
+		List<Jugador> jugadors = jugadorDao.listar();
+		Collections.shuffle(jugadors);
+
+		while (!FiPartida) {
+
+			System.out.println("Es el torn del jugador " + jugadors.get(0));
+			Jugador jugadorActiu = jugadors.get(0);
+			TirarDaus(jugadorActiu);
 		}
 	}
-	
-	
-	private void SeleccionarTorn(){
-		//cal comprobar si Tokio está lliure si cap
-		//mosntre está a tokyo llavors el mosntre actual entra
-		TirarDaus();
-	}
-	
-	private void TirarDaus() {
-		//Función de simulacion de tiradas de dados	
-		ArrayList<Integer>resultadosDados= new ArrayList<Integer>();
+
+	private void TirarDaus(Jugador JugActiu) {
+
+		ArrayList<Integer> resultadosDados = new ArrayList<Integer>();
 		for (int i = 0; i < 6; i++) {
 			resultadosDados.add(0);
 		}
-		int var=0;
-		Random azar= new Random();
+		int var = 0;
+		Random azar = new Random();
 		int dau;
 		for (int i = 0; i < 6; i++) {
-			dau= azar.nextInt(0,6+1);
-			
+			dau = azar.nextInt(0, 6 + 1);
+
 			switch (dau) {
-			case 1: 
+			case 1:
 				var = resultadosDados.get(0) + 1;
 				resultadosDados.set(0, var);
 				break;
@@ -82,32 +79,39 @@ public class StartGame {
 				resultadosDados.set(5, var);
 				break;
 			}
-			
+
 		}
+
+		System.out.println("Los resultados de la tirada són " + resultadosDados.get(0) + " unos, "
+				+ resultadosDados.get(1) + " doses, " + resultadosDados.get(2) + " treses, " + resultadosDados.get(3)
+				+ " de energia, " + resultadosDados.get(4) + " garras i " + resultadosDados.get(5) + " corazones.");
+
+		resoldreTirada(resultadosDados, JugActiu);
+
+	}
+
+	// cal comprobar si Tokio está lliure si cap
+	// mosntre está a tokyo llavors el mosntre actual entra
+	// Función de simulacion de tiradas de dados
+	private void resoldreTirada(ArrayList<Integer> resultados, Jugador JugActiu) {
 		
-		System.out.println("Los resultados de la tirada són "+resultadosDados.get(0)+" unos, "+resultadosDados.get(1)+" doses, "+resultadosDados.get(2)+" treses, "
-		+resultadosDados.get(3)+" de energia, "+resultadosDados.get(4)+" garras i "+resultadosDados.get(5)+" corazones.");
+		
 		
 
 	}
-		//resoldreTirada(Jugador jugador);
-		//comunica con clase monstre para que al jugador habil se le cambie los stats dependienddo de la tirada
-		
-		/*
-		 * Sumar puntos de victoria.
-		 * Sumar Vida
-		 * Hacer Daño a los otros monstruos
-		 * Si has pegado y no eres el monstruo de tokyo y el monstruo de tokyo esta vivo se ha de pregunatr si el monstruo de tokyo se quiere ir
-		 * Si has matado al monstruo de Tokyo has de ser el monstruo de Tokyo
-		 * Ganar Energia
-		*/
-		//Una vez resuelta la tira se debe comprobar:
-		/*Cuanto poder teiens
-		 * Condiciones de victoria: si eres el unico monstruo vivo o bien si tus puntis de victoria suben a 20 o mas.
-		*/
-		
+	// comunica con clase monstre para que al jugador habil se le cambie los stats
+	// dependienddo de la tirada
 
-	
-	
+	/*
+	 * Sumar puntos de victoria. Sumar Vida Hacer Daño a los otros monstruos Si has
+	 * pegado y no eres el monstruo de tokyo y el monstruo de tokyo esta vivo se ha
+	 * de pregunatr si el monstruo de tokyo se quiere ir Si has matado al monstruo
+	 * de Tokyo has de ser el monstruo de Tokyo Ganar Energia
+	 */
+	// Una vez resuelta la tira se debe comprobar:
+	/*
+	 * Cuanto poder teiens Condiciones de victoria: si eres el unico monstruo vivo o
+	 * bien si tus puntis de victoria suben a 20 o mas.
+	 */
 
 }
